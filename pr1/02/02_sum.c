6bcce717ea8b01c11454c9393eb430bc9a941020
melnichenkodmitry <dima.mel99@mail.ru>
2019-02-22 04:06:09 +0700

    files is empty

---
+++ pr1/02/02_sum.c
@@ -0,0 +1,11 @@
+#include <assert.h>
+#include <stdio.h>
+int main (int argc, char const *argv[])
+{
+	int x,y;
+	scanf("%d", &x);
+	scanf("%d", &y);
+	printf("%d\n", x + y);
+	return 0;
+}
+	
---
+++ pr1/03/03_pow.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+int x,y;
+scanf("%d", &x);
+scanf("%d", &y);
+printf("%f", pow(x,y));
+return 0;
+}
